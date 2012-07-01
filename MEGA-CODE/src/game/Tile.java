708c85c4a32e3e7e5fae70debdbdf0394f146893
Dylan Swiggett <omnipotentperson@gmail.com>
2012-06-30 17:11:40 -0700

    Added path finding.

--- MEGA-CODE/src/game/Tile.java
+++ MEGA-CODE/src/game/Tile.java
@@ -7,6 +7,7 @@ import mvc.RectSprite;
 public class Tile extends RectSprite{
 	
 	boolean highGround = false;
+	boolean onPath = false;
 	
 	public static final int lowGroundHeight = 0;
 	public static final int highGroundHeight = 0;
@@ -15,10 +16,18 @@ public class Tile extends RectSprite{
 	
 	public Tile(int x, int y, float tileWidth, float tileHeight, boolean highGround){
 		super(x * tileWidth, y * tileHeight, tileWidth, tileHeight, highGround ? highGroundHeight : lowGroundHeight , highGround ? Color.LIGHT_GRAY : Color.DARK_GRAY);
+		
+		this.highGround = highGround;
 	}
 	
 	public void draw(){
 		super.draw();
+		
+		if (onPath){
+			setColor(Color.GREEN);
+		} else {
+			setColor(highGround ? Color.LIGHT_GRAY : Color.DARK_GRAY);
+		}
 		
 		if (tower != null){
 			tower.draw();
