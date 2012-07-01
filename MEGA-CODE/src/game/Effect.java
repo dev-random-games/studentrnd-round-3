5c6f97b63fb5b7a027ca1b88999abd298bb8aff4
leahcim <leahcim@leahcim-laptop3.mshome.net>
2012-07-01 08:07:11 -0700

    And some more stuff.

---
+++ MEGA-CODE/src/game/Effect.java
@@ -0,0 +1,13 @@
+package game;
+
+import mvc.TextureSprite;
+
+public class Effect extends TextureSprite {
+	
+	public Effect(float x, float y, float w, float h, float depth, String texturePath) {
+		super(x, y, w, h, depth, texturePath);
+		
+	}
+	
+	
+}
