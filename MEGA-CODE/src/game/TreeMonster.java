332792e02d04dd11307f3d0309658b68ad82105e
Lane Aasen <laneaasen@gmail.com>
2012-07-01 02:55:00 -0700

    Added new monster types. awaiting graphics.

---
+++ MEGA-CODE/src/game/TreeMonster.java
@@ -0,0 +1,10 @@
+package game;
+
+public class TreeMonster extends Monster {
+	
+	static String[] texturePaths = {"data/tree0.png, data/tree1.png, data/tree2.png"};
+	
+	public TreeMonster(float x, float y, float tileWidth, float tileHeight) {
+		super(x, y, tileWidth, tileHeight, 200, 2, 0, 2, texturePaths);
+	}
+}
