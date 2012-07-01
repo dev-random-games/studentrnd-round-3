0b31f14f3f2a3e9e1d98ec49f5ec18121615509a
leahcim <leahcim@leahcim-laptop3.home>
2012-07-01 11:38:26 -0700

    fixin

--- MEGA-CODE/src/mvc/Client.java
+++ MEGA-CODE/src/mvc/Client.java
@@ -25,7 +25,7 @@ public class Client extends Thread{
 	public ObjectInputStream in;
 	
 	String host = "localhost";
-//	String host = "192.168.1.2";
+	//String host = "192.168.1.2";
 	int port = 12345;
 	
 	int userId;
