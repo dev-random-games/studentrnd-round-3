d11a8943ef471ae0ef57ce0fad1ace7011f83ff2
leahcim <leahcim@leahcim-laptop3.home>
2012-07-01 13:09:35 -0700

    Adding Effect

---
+++ MEGA-CODE/src/mvc/Effect.java
@@ -0,0 +1,39 @@
+package mvc;
+
+import org.lwjgl.opengl.GL11;
+
+public class Effect {
+	
+	public float x, y, endX, endY;
+	public int duration;
+	
+	public Effect(float x, float y, float endX, float endY, int duration) {
+		this.x = x;
+		this.y = y;
+		this.endX = endX;
+		this.endY = endY;
+		this.duration = duration;
+	}
+	
+	public void draw() {
+		System.out.println(duration);
+		this.duration--;
+		GL11.glColor3f(255, 0, 0);
+		float w = 10;
+		float depth = 50;
+		float h = 10;
+//		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+		GL11.glBegin(GL11.GL_LINES);
+		GL11.glVertex3f(x, y, depth);
+		GL11.glVertex3f(endX, endY, depth);
+//		GL11.glVertex3f(x, y, depth);
+//		GL11.glVertex3f(x + w, y, depth);
+//		GL11.glVertex3f(x + w, y + h, depth);
+//		GL11.glVertex3f(x, y + h, depth);
+		GL11.glEnd();
+	}
+	
+	public boolean shouldDie() {
+		return this.duration <= 0;
+	}
+}
